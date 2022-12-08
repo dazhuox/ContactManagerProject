739fcc88463270c0016210a8431171fa25ae31bc
MaximG01 <102536881+MaximG01@users.noreply.github.com>
2022-12-08 09:12:41 -0500

    Skeleton :)

--- ContactManagerProject/Window1.xaml.cs
+++ ContactManagerProject/Window1.xaml.cs
@@ -10,38 +10,21 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ContactManagerProject
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
-            // This shows the window.
             InitializeComponent();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void AddContactDoubleClick(object sender, RoutedEventArgs e)
         {
 
         }
