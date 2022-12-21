9da47030dee9c2f472c7f151cf9512747fef380c
MaximG01 <102536881+MaximG01@users.noreply.github.com>
2022-12-20 22:34:25 -0500

    CSV completed

--- ContactManagerProject/MainWindow.xaml.cs
+++ ContactManagerProject/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -78,41 +79,146 @@ namespace ContactManagerProject
         {
             //connection
             string connectionString = "Server=localhost;Database=dotNetProject;Trusted_Connection=True;";
-            string queryString = "SELECT ID, ContactImage_ID, FirstName, MiddleName, LastName, Salutation, CreateDateTime, UpdateDateTime FROM CONTACT"; //add other tables
+            string queryStringContact = "SELECT ID, ContactImage_ID, FirstName, MiddleName, LastName, Salutation, CreateDateTime, UpdateDateTime FROM CONTACT";
+            string queryStringAddress = "SELECT ID, Contact_ID, Type, Country, City, Street, AddressNumber FROM ADDRESS";
+            string queryStringEmail = "SELECT ID, Contact_ID, Type, EmailAddress FROM EMAIL";
+            string queryStringPhone = "SELECT ID, Contact_ID, Type, Number, CreateDateTime, UpdateDateTime FROM PHONE";
+            string queryStringType = "SELECT Code, Description FROM TYPE";
+
+
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                SqlCommand command = new SqlCommand(queryString, connection);
+                //everything is for the 5 tables
+                SqlCommand commandContact = new SqlCommand(queryStringContact, connection);
+                SqlCommand commandAddress = new SqlCommand(queryStringAddress, connection);
+                SqlCommand commandEmail = new SqlCommand(queryStringEmail, connection);
+                SqlCommand commandPhone = new SqlCommand(queryStringPhone, connection);
+                SqlCommand commandType = new SqlCommand(queryStringType, connection);
+
                 connection.Open();
 
-                SqlDataReader reader = command.ExecuteReader();
+                SqlDataReader Contact = commandContact.ExecuteReader();
+                //SqlDataReader ReaderAddress = commandAddress.ExecuteReader();
+                //SqlDataReader readerEmail = commandEmail.ExecuteReader();
+                //SqlDataReader readerPhone = commandPhone.ExecuteReader();
+                //SqlDataReader readerType = commandType.ExecuteReader();
 
                 //load data
-                DataTable dataTable = new DataTable();
-                dataTable.Load(reader);
+                DataTable dataTableContact = new DataTable();
+                DataTable dataTableAddress = new DataTable();
+                DataTable dataTableEmail = new DataTable();
+                DataTable dataTablePhone = new DataTable();
+                DataTable dataTableType = new DataTable();
+
+                dataTableContact.Load(Contact);
+                dataTableAddress.Load(Contact);
+                dataTableEmail.Load(Contact);
+                dataTablePhone.Load(Contact);
+                dataTableType.Load(Contact);
 
                 //StringBuilder to get it all together(CSV)
                 StringBuilder sb = new StringBuilder();
 
-                //Add column names
+                //Add column names (contact)
                 string[] columnNames = new String[] { "ID", "ContactImage_ID", "FirstName", "MiddleName", "LastName", "Salutation", "CreateDateTime", "UpdateDateTime" };
                 sb.AppendLine(string.Join(",", columnNames));
 
                 //Add Rows
-                foreach (DataRow row in dataTable.Rows)
+                foreach (DataRow row in dataTableContact.Rows)
                 {
                     string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
                     sb.AppendLine(string.Join(",", fields));
 
                 }
 
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "CSV File|*.csv";
-                saveFileDialog.Title = "Save CSV File";
-                saveFileDialog.ShowDialog();
+                //Add column names (address)
+                columnNames = new String[] { "ID", "Contact_ID", "Type", "Country", "City", "Street", "AddressNumber" };
+                sb.AppendLine(string.Join(",", columnNames));
 
-                // Write the CSV data to the selected file
-                System.IO.File.WriteAllText(saveFileDialog.FileName, sb.ToString());
+                //Add Rows
+                foreach (DataRow row in dataTableContact.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    sb.AppendLine(string.Join(",", fields));
+
+                }
+
+                //Add column names (Email)
+                columnNames = new String[] { "ID", "Contact_ID", "Type", "EmailAddress" };
+                sb.AppendLine(string.Join(",", columnNames));
+
+                //Add Rows
+                foreach (DataRow row in dataTableContact.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    sb.AppendLine(string.Join(",", fields));
+
+                }
+
+                //Add column names (Phone)
+                columnNames = new String[] { "SELECT ID", "Contact_ID", "Type", "Number", "CreateDateTime", "UpdateDateTime" };
+                sb.AppendLine(string.Join(",", columnNames));
+
+                //Add Rows
+                foreach (DataRow row in dataTableContact.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    sb.AppendLine(string.Join(",", fields));
+
+                }
+
+                //Add column names (Type)
+                columnNames = new String[] { "SELECT Code", "Description" };
+                sb.AppendLine(string.Join(",", columnNames));
+
+                //Add Rows
+                foreach (DataRow row in dataTableContact.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    sb.AppendLine(string.Join(",", fields));
+                }
+
+                foreach (DataRow row in dataTableAddress.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    sb.AppendLine(string.Join(",", fields));
+                }
+
+                foreach (DataRow row in dataTableEmail.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    sb.AppendLine(string.Join(",", fields));
+                }
+
+                foreach (DataRow row in dataTablePhone.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    sb.AppendLine(string.Join(",", fields));
+                }
+
+                foreach (DataRow row in dataTableType.Rows)
+                {
+                    string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                    sb.AppendLine(string.Join(",", fields));
+                }
+
+
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    File.WriteAllText(filePath, sb.ToString());
+                    Console.WriteLine("Data exported successfully!");
+                }
+
+
+
+                connection.Close();
 
             }
         }
